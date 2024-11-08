d199791a3578040c9717990b0789f497c158c062
Juliane Reuter <julianereuter@Julianes-MacBook-Pro.local>
2024-11-08 20:49:27 +0100

    add backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-const apiUrl = "/choreo-apis/awbo/backend/rest-api-be2/v1.0";
+const apiUrl = "/choreo-apis/djangoreacttutorial/backend/v1";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
-  // baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
+  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
 });
 
 api.interceptors.request.use(
